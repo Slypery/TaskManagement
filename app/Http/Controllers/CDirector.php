8a3de42c2f3e1fbffe00d85ed3ba03d7a56731b7
Adhizy <firstaadiwidjaya@gmail.com>
2024-11-20 11:27:20 +0700

    User

--- app/Http/Controllers/CDirector.php
+++ app/Http/Controllers/CDirector.php
@@ -14,13 +14,37 @@ class CDirector extends Controller
         ]);
     }
 
-    public function user_list() {
+    public function user_list()
+    {
         return view('user_list', [
             'page_name' => 'User List',
             'user_list' => User::get()
         ]);
     }
-    public function store_user(Request $request) {}
-    public function update_user(Request $request) {}
-    public function destroy_user(Request $request) {}
+    public function store_user(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'email' => 'required|email',
+            'role' => 'required',
+            'password' => 'required'
+        ]);
+        User::create($request->all());
+        return redirect()->route('director.user_list.index');
+    }
+    public function update_user(Request $request, User $user)
+    {
+        $request->validate([
+            'username' => 'required',
+            'email' => 'required|email',
+            'role' => 'required'
+        ]);
+        $user->update($request->all());
+        return redirect()->route('director.user_list.index');
+    }
+    public function destroy_user(Request $request, User $user)
+    {
+        $user->delete();
+        return redirect()->route('director.user_list.index');
+    }
 }
--- resources/views/user_list.blade.php
+++ resources/views/user_list.blade.php
@@ -3,26 +3,62 @@
     <x-director_sidebar pageName="{{ $page_name }}" />
 @endsection
 @section('main')
-<div class="table-style">
-    <table>
-        <thead>
-            <tr>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($user_list as $item)
+    {{-- form add --}}
+    <form action="{{ route('director.user_list.store') }}" method="post">
+        @csrf
+        <input type="text" id="username" name="username" placeholder="Username" required>
+        <input type="email" id="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" id="password" placeholder="Password" required>
+        <select name="role" id="role" required>
+            <option value="" disabled selected>--none--</option>
+            <option value="director">Director</option>
+            <option value="manager">Manager</option>
+            <option value="employee">Employee</option>
+        </select>
+        <button type="submit">Save</button>
+    </form>
+
+    <div class="table-style">
+        <table>
+            <thead>
                 <tr>
-                    <td>{{ $item->username }}</td>
-                    <td>{{ $item->email }}</td>
-                    <td>{{ $item->role }}</td>
-                    <td></td>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Action</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                @foreach ($user_list as $item)
+                    <tr>
+                        <td>{{ $item->username }}</td>
+                        <td>{{ $item->email }}</td>
+                        <td>{{ $item->role }}</td>
+                        <td>
+                            {{-- button delete --}}
+                            <form action="{{ route('director.user_list.destroy', $item->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button style="color: red" type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+
+                    {{-- form edit --}}
+                    <form action="{{ route('director.user_list.update', $item->id) }}" method="post">
+                        @csrf
+                        @method('PUT')
+                        <input type="text" id="username" name="username" placeholder="Username" value="{{ $item->username }}" required>
+                        <input type="email" id="email" name="email" placeholder="Email" value="{{ $item->email }}" required>
+                        <select name="role" id="role" required>
+                            <option value="director" {{ $item->role == 'director' ? 'selected' : '' }}>Director</option>
+                            <option value="manager" {{ $item->role == 'manager' ? 'selected' : '' }}>Manager</option>
+                            <option value="employee" {{ $item->role == 'employee' ? 'selected' : '' }}>Employee</option>
+                        </select>
+                        <button type="submit">Save</button>
+                    </form><br>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
